f383c703c8c0a2b1b950abd2405844bc311c96ed
rizziemma <rizziemma22@gmail.com>
2020-12-16 19:50:48 +0100

    donut diagram

--- analyse.r
+++ analyse.r
@@ -28,7 +28,6 @@ ggplot(data=datasLength, aes(x=factor(Length), y=x, fill=Status))
 ##Analyse categories
 #succès par catégorie
 success <- ddply(file, .(main_category), summarize, rate=sum(status=="successful")*100/length(status))
-success
 
 ggplot(data = success, aes(x=main_category, y=rate)) + 
   geom_bar(stat="identity", position=position_dodge())
@@ -36,6 +35,21 @@ ggplot(data = success, aes(x=main_category, y=rate)) +
 #TODO
 #trier + couleurs
 
+
 #projets par pays -> DONUT
+data <- aggregate(file$id, by=list(Country=file$country), FUN=length)
+data <- data[order(- data$x),]
+country <- data[1:5,] 
+levels(country$Country) <- c(levels(country$Country), "Others")
+country[6,] <- c("Others", sum(data[-(1:5),]$x))
+country$x <- as.numeric(country$x)
+country$fraction = country$x / sum(country$x)
+country$ymax = cumsum(country$fraction)
+country$ymin = c(0, head(country$ymax, n=-1))
+ggplot(country, aes(ymax=ymax, ymin=ymin, xmax=4, xmin=3, fill=Country)) +
+  geom_rect() +
+  coord_polar(theta="y") +
+  xlim(c(2,4))
+
 #succés par catégorie pour chaque pays 
 
